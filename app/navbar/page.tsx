8b969b24215c16a69fc3fcd9cd4a047dcb98fcca
chlxyz <chloechloechronicleuwu@gmail.com>
2023-12-27 06:47:37 +0700

    # On branch master # Changes to be committed: #	modified:   app/globals.css #	new file:   app/home/page.tsx #	new file:   app/navbar/page.tsx #	modified:   app/page.tsx #	new file:   public/fonts/Betatron.otf #	new file:   public/images/etheral.png #	new file:   public/images/ethereal.gif

---
+++ app/navbar/page.tsx
@@ -0,0 +1,10 @@
+export default function Navbar(){
+    return (
+        <nav className="navbar">
+            <ul className="nav flex float-left">
+                <li className="text-white justify-center mx-[auto] my-[10%] ml-4 bg-black rounded-md pr-2 pl-2">C. R. E. D.</li>
+                <li className="my-[10%] ml-48">OCREON</li>
+            </ul>
+        </nav>
+    )
+}
